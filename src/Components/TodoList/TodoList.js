7da6b203b58adb4d00c1a4d663a4a999f487d237
Julsperez <julsperez@outlook.com>
2026-03-03 15:26:21 -0600

    Responsive design

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -4,36 +4,12 @@ import { HiCheck } from "react-icons/hi";
 import { ListDivider, TodoItem } from "../";
 import "./TodoList.css";
 
-// const FILTER_STRATEGIES = {
-//   ALL: {
-//     id: 0,
-//     label: "Todas las misiones",
-//     filterFn: (todo) => (todo.status === "archived" || todo.isCompleted),
-//   },
-//   COMPLETED: {
-//     id: 1,
-//     label: "Misiones completadas",
-//     filterFn: (todo) => todo.isCompleted && todo.status !== "archived",
-//   },
-//   ARCHIVED: {
-//     id: 2,
-//     label: "Misiones archivadas",
-//     filterFn: (todo) => todo.status === "archived",
-//   },
-// };
-
-// function TodoList({ onTodoUpdated, searchedTodos }) {
 function TodoList() {
   const { setTodos, searchedTodos, todos } = React.useContext(TodoContext);
-  // remove missions from main and side if they are completed
-  // const mainMissions = todos.filter(todo => todo.typeofMission === "main");
-  // const sideMissions = todos.filter(todo => todo.typeofMission === "side");
-  // const completedMissions = todos.filter(todo => todo.isCompleted);
-
   const [pillIndex, setPillIndex] = React.useState(0);
   const [filterTodos, setFilterTodos] = React.useState(searchedTodos);
   const [dividerText, setDividerText] = React.useState("Todas las misiones");
-  
+
   const headerRef = useRef(null);
   const [isPinned, setIsPinned] = React.useState(false);
   useEffect(() => {
@@ -48,16 +24,15 @@ function TodoList() {
         threshold: [1],
         // El margin negativo en el top fuerza la activación 
         // justo cuando toca el borde superior
-        rootMargin: '-9px 0px 0px 0px', 
+        rootMargin: '-9px 0px 0px 0px',
       }
     );
 
     observer.observe(cachedRef);
 
-    // Limpieza al desmontar el componente
     return () => observer.disconnect();
   }, []);
-  
+
   useEffect(() => {
     handleSelectedPill(pillIndex);
   }, [searchedTodos]);
@@ -78,12 +53,6 @@ function TodoList() {
     setPillIndex(pillIndex);
   };
 
-  // const activeStrategy = Object.values(FILTER_STRATEGIES).find(s => s.id === pillIndex) || FILTER_STRATEGIES.ALL;
-  // useMemo(() =>{
-  //   setDividerText(activeStrategy.label);
-  //   setFilterTodos(searchedTodos.filter(activeStrategy.filterFn));
-  // }, [searchedTodos, activeStrategy]);
-
   const updateTodos = (updatedTodo) => {
     let newTodos;
     if (updatedTodo.status === "deleted") {
@@ -98,23 +67,23 @@ function TodoList() {
 
   return (
     <div className="todoListContainer">
-      <div ref={headerRef} className={`todoListHeader ${isPinned ? 'is-pinned': ''}`}>
+      <div ref={headerRef} className={`todoListHeader ${isPinned ? 'is-pinned' : ''}`}>
         {
-          [0,1,2].map(index => (
-            <span key={index} 
-              className={`headerPill ${pillIndex === index ? "active" : ""}`} 
+          [0, 1, 2].map(index => (
+            <span key={index}
+              className={`headerPill ${pillIndex === index ? "active" : ""}`}
               onClick={() => { handleSelectedPill(index); }}>
-                {index === 0 ? "Todas" : index === 1 ? "Completadas" : "Archivadas"}
-                {pillIndex === index && (<HiCheck className="check-icon" />)}
+              {index === 0 ? "Todas" : index === 1 ? "Completadas" : "Archivadas"}
+              {pillIndex === index && (<HiCheck className="check-icon" />)}
             </span>
           ))
         }
       </div>
       <div className="todoListPanel">
-        <ListDivider dividerText={dividerText}/>
+        <ListDivider dividerText={dividerText} />
         {
           !!filterTodos.length && (
-            <>
+            <div className="todoListPanelContent">
               {filterTodos.map(todo => (
                 <React.Fragment key={todo.missionId}>
                   <TodoItem
@@ -123,34 +92,10 @@ function TodoList() {
                   />
                 </React.Fragment>
               ))}
-            </>
+            </div>
           )
         }
       </div>
-
-
-      {/* <ListDivider dividerText={"Misiones secundarias"}/>
-      {
-        (!!sideMissions.length ? (
-          <>
-            {sideMissions.map(todo => (
-              <TodoItem key={todo.missionId} todo={todo}/>
-            ))}
-          </>
-        ): null )
-      }
-
-      <ListDivider dividerText={"Misiones completadas"}/>
-      {
-        (!!completedMissions.length ? (
-          <>
-            {completedMissions.map(todo => (
-              <TodoItem key={todo.missionId} todo={todo}/>
-            ))}
-          </>
-        ): null )
-      } */}
-      
     </div>
   );
 }
